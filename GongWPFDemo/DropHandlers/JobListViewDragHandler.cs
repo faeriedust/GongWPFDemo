1861241a25fb8438e256bbc74395be267492f5e2
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-05-19 10:14:24 +0900

    Set up drag handlers

---
+++ GongWPFDemo/DropHandlers/JobListViewDragHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using GongSolutions.Wpf.DragDrop;
+using GongWPFDemo.ViewModels;
+
+namespace GongWPFDemo.DropHandlers {
+    internal class JobListViewDragHandler : IDropTarget {
+        public JobListViewDragHandler() { }
+        public bool CanDrop(IDropInfo dropInfo) {
+            return dropInfo.Data is JobViewModel;
+        }
+        public void DragOver(IDropInfo dropInfo) {
+            if (this.CanDrop(dropInfo)) {
+                dropInfo.Effects = DragDropEffects.Move;
+                dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+            } else {
+                dropInfo.Effects = DragDropEffects.None;
+            }
+        }
+        public void Drop(IDropInfo dropInfo) {
+            if (this.CanDrop(dropInfo)) {
+                var myCollection = (ICollection<JobViewModel>) dropInfo.TargetCollection;
+                var insertionItem = (JobViewModel) dropInfo.Data;
+            }
+        }
+    }
+}
